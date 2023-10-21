b9210265f02d829e33019e654b11e6eb9d99256a
MuhammadDzakiFawwazArdika <118704683+MuhammadDzakiFawwazArdika@users.noreply.github.com>
2023-10-21 07:58:11 +0700

    Ini sidebar ndess

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,51 +3,58 @@
 @section('title', 'Register')
 
 @section('content')
-{{-- <section class="min-vh-25 mb-8">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('../soft-ui-dashboard-main/assets/img/curved-images/curved14.jpg');">
-        <span class="mask bg-gradient-dark opacity-6"></span>
+    <section class="min-vh-25 mb-8">
+        <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+            style="background-image: url('../soft-ui-dashboard-main/assets/img/curved-images/curved14.jpg');">
+            <span class="mask bg-gradient-dark opacity-6"></span>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 text-center mx-auto">
+                        <h1 class="text-white mb-2 mt-5">Selamat Datang!</h1>
+                        <p class="text-lead text-white">lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-5 text-center mx-auto">
-                    <h1 class="text-white mb-2 mt-5">Selamat Datang!</h1>
-                    <p class="text-lead text-white">lorem ipsum dolor sit amet consectetur.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row mt-lg-n10 mt-md-n11 mt-n10">
-            <div class="col-xl-6 col-lg-5 col-md-7 mx-auto">
-                <div class="card z-index-0">
-                    <div class="card-header text-center pt-4">
-                        <h5>Register Here</h5>
-                    </div>
-                    <div class="card-body">
-                        <form role="form text-left">
-                            <div class="mb-3">
-                                <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="email-addon">
-                            </div>
-                            <div class="mb-3">
-                                <input type="text" class="form-control" placeholder="Enter Your Phone" aria-label="Email" aria-describedby="email-addon">
-                            </div>
-                            <div class="mb-3">
-                                <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
-                            </div>
-                            <div class="form-check form-check-info text-left">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    I agree the <a href="" class="text-dark font-weight-bolder">Terms and Conditions</a>
-                                </label>
-                            </div>
-                            <div class="text-center">
-                                <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
-                            </div>
-                            <p class="text-sm mt-3 mb-0">Sudah punya akun? <a href="" class="text-dark text-gradient font-weight-bolder">Sign in</a></p>
-                        </form>
+            <div class="row mt-lg-n10 mt-md-n11 mt-n10">
+                <div class="col-xl-6 col-lg-5 col-md-7 mx-auto">
+                    <div class="card z-index-0">
+                        <div class="card-header text-center pt-4">
+                            <h5>Register Here</h5>
+                        </div>
+                        <div class="card-body">
+                            <form role="form text-left">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control" placeholder="Name" aria-label="Name"
+                                        aria-describedby="email-addon">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" class="form-control" placeholder="Enter Your Phone"
+                                        aria-label="Email" aria-describedby="email-addon">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="password" class="form-control" placeholder="Password" aria-label="Password"
+                                        aria-describedby="password-addon">
+                                </div>
+                                <div class="form-check form-check-info text-left">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                                        checked>
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        I agree the <a href="" class="text-dark font-weight-bolder">Terms and
+                                            Conditions</a>
+                                    </label>
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                                </div>
+                                <p class="text-sm mt-3 mb-0">Sudah punya akun? <a href=""
+                                        class="text-dark text-gradient font-weight-bolder">Sign in</a></p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    </section> --}}
+    </section>
 @endsection
