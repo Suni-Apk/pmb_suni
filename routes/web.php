ce387203997f773327ee3fedb05d2b4718d75e23
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-10-21 21:30:13 +0700

    sementara fix aside

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,10 @@ Route::prefix('template')->group(function () {
         return view('layouts.template.form');
     })->name('form');
 
+    Route::get('/forgot', function () {
+        return view('layouts.template.forgot-password');
+    })->name('forgot');
+
     Route::get('/profile', function () {
         return view('layouts.template.profile');
     })->name('profile');
