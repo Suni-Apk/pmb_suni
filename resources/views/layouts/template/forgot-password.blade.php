fdb860ff7a3ffeb6cf28d4fed4a7161ba0605158
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-10-21 21:30:13 +0700

    sementara fix aside

--- resources/views/layouts/template/forgot-password.blade.php
+++ resources/views/layouts/template/forgot-password.blade.php
@@ -12,7 +12,7 @@
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="card">
                         <div class="card-body">
-                            <form method="POST" action="#">
+                            <form method="POST" action="{{ route('password.email') }}">
                                 @csrf
                                 <div class="row mb-3">
                                     <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
