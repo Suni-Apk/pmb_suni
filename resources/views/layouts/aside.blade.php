79bf73c2b49b725391a573ec3002b5c0457ebbd7
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-10-22 09:50:39 +0700

    check

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -10,40 +10,23 @@
         </a>
     </div>
     <hr class="horizontal dark mt-0">
-    @if (Auth::user()->role == 'Admin')
-        <div class="collapse navbar-collapse w-auto min-vh-70" id="sidenav-collapse-main">
-            <ul class="navbar-nav overflow-x-hidden" id="accordionSidebar">
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{ route('admin.dashboard') }}">
-                        <div
-                            class="icon icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="fas fa-th-large"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Dashboard</span>
-                    </a>
-                </li>
+    <div class="collapse navbar-collapse w-auto min-vh-70" id="sidenav-collapse-main">
+        <ul class="navbar-nav overflow-x-hidden" id="accordionSidebar">
+            <li class="nav-item">
+                <a class="nav-link active" href="{{ route('admin.dashboard') }}">
+                    <div
+                        class="icon icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="fas fa-th-large"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
 
-                <!-- kuliah -->
-                <li class="nav-item">
-                    <a data-bs-toggle="collapse" href="#university" class="nav-link" aria-controls="university" role="button" aria-expanded="false">
-                        <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
-                            <i class="fas fa-university"></i>
-                            <title>credit-card</title>
-                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                                    <g transform="translate(1716.000000, 291.000000)">
-                                        <g transform="translate(453.000000, 454.000000)">
-                                            <path class="color-background opacity-6"
-                                                d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z">
-                                            </path>
-                                            <path class="color-background"
-                                                d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-                                            </path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
+            <!-- kuliah -->
+            <li class="nav-item">
+                <a data-bs-toggle="collapse" href="#university" class="nav-link" aria-controls="university" role="button" aria-expanded="false">
+                    <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
+                        <i class="fas fa-university"></i>
                     </div>
                     <span class="nav-link-text ms-1">University</span>
                     </span>
@@ -148,91 +131,53 @@
                 </div>
             </li>
 
-            <!-- mata kuliah -->
-            <li class="nav-item">
-                <a data-bs-toggle="collapse" href="#matkul" class="nav-link" aria-controls="matkul" role="button" aria-expanded="false">
-                    <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
-                        <i class="ni ni-hat-3"></i>
-                    </div>
-                    <span class="nav-link-text ms-1"> Mata Kuliah </span>
-                </a>
-                <div class="collapse" id="matkul">
-                    <ul class="nav ms-4 ps-3">
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-border-all"></i></span>
-                                <span class="sidenav-normal"> Daftar Mata Kuliah </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-plus"></i></span>
-                                <span class="sidenav-normal"> Tambah Mata Kuliah </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            
-            {{-- users --}}
-            <li class="nav-item">
-                <ul class="nav-link pb-0 mb-0">
-                    <span class="sidenav-mini-icon d-none d-xl-block" style="color:rgb(196, 196, 196)"><i class="fas fa-users"></i></span>
-                    <span class="sidenav-normal text-uppercase text-xs ms-2 font-weight-bolder"> users </span>
-                </ul>
-            </li>
+                <!--mahasiswa-->
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('billing') }}">
+                        <div
+                            class="icon icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-user-graduate"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Mahasiswa</span>
+                    </a>
+                </li>
 
-            <!-- admin -->
-            <li class="nav-item">
-                <a data-bs-toggle="collapse" href="#admin" class="nav-link" aria-controls="admin" role="button" aria-expanded="false">
-                    <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
-                        <i class="fas fa-user-cog"></i>
+                <!--admin-->
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('table') }}">
+                        <div
+                            class="icon icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-user-cog"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Admin</span>
+                    </a>
+                </li>
+                
+                <!-- biaya -->
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#payment" class="nav-link " aria-controls="payment" role="button" aria-expanded="false">
+                        <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
+                            <i class="ni ni-credit-card"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Biaya</span>
+                    </a>
+                    <div class="collapse " id="payment">
+                        <ul class="nav ms-4 ps-3">
+                            <li class="nav-item ">
+                                <a class="nav-link " href="">
+                                    <span class="sidenav-mini-icon"><i class="fas fa-wallet"></i></span>
+                                    <span class="sidenav-normal"> Biaya Tagihan </span>
+                                </a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link " href="">
+                                    <span class="sidenav-mini-icon"><i class="ni ni-ungroup"></i></span>
+                                    <span class="sidenav-normal"> Biaya Bawaan </span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <span class="nav-link-text ms-1"> Admin </span>
-                </a>
-                <div class="collapse" id="admin">
-                    <ul class="nav ms-4 ps-3">
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-border-all"></i></span>
-                                <span class="sidenav-normal"> Daftar Admin </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-plus"></i></span>
-                                <span class="sidenav-normal"> Tambah Admin </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-
-            <!-- mahasiswa -->
-            <li class="nav-item">
-                <a data-bs-toggle="collapse" href="#mahasiswa" class="nav-link" aria-controls="mahasiswa" role="button" aria-expanded="false">
-                    <div class="icon icon-sm shadow border-radius-md bg-white text-center d-flex align-items-center justify-content-center  me-2">
-                        <i class="fas fa-user-graduate"></i>
-                    </div>
-                    <span class="nav-link-text ms-1"> Mahasiswa </span>
-                </a>
-                <div class="collapse" id="mahasiswa">
-                    <ul class="nav ms-4 ps-3">
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-border-all"></i></span>
-                                <span class="sidenav-normal"> Daftar Mahasiswa </span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link " href="">
-                                <span class="sidenav-mini-icon d-none d-xl-block"><i class="fas fa-plus"></i></span>
-                                <span class="sidenav-normal"> Tambah Mahasiswa </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+                </li>
 
             {{-- payment --}}
             <li class="nav-item">
