597cc7560bf4b2fc1de9bfa9ee5d91019abefacf
farhan181206 <farhanmuti54@gmail.com>
2023-10-22 20:10:49 +0700

    terbaru

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,9 @@
     <main class="main-content position-relative min-height-screen h-auto border-radius-lg">
 
         <div class="z-3 position-relative">
-            @include('layouts.header')
+            {{-- @include('layouts.header') --}}
+            @component('layouts.header')
+            @endcomponent
         </div>
         
         {{-- @include('layouts.template') --}}
