4a82db9a6f6304bc80d34cdcc95ccd3c1bfa1ec4
Alegrich <140672362+Alegrich@users.noreply.github.com>
2023-10-26 10:37:39 +0700

    baru nih coy hijriyah

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,7 +27,7 @@
       </div>
     </div>
     <div class="col-xl-3 col-sm-6">
-      <div class="card">
+      {{-- <div class="card">
         <div class="card-body p-3">
           <div class="row">
             <div class="col-8">
@@ -41,35 +41,29 @@
             </div>
             <div class="col-4 text-end">
               <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
+              <i class="ni ni-money text-lg opacity-10" aria-hidden="true"></i>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </div> --}}
     </div>
   </div>
   <div class="row mt-4">
-    <div class="col-lg-7 mb-lg-0 mb-4">
+    <div class="col-lg-8 mb-lg-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
           <div class="row">
             <div class="col-lg-6">
-              <div class="d-flex flex-column h-100">
-                <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
-                <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
-                <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="">
-                  Read More
-                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
-                </a>
+              <div class="">
+                <h5 class="font-weight-bolder mt-5 p-5">Selamat Datang {{ $user->name }}</h5>
               </div>
             </div>
             <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
-              <div class="bg-gradient-primary border-radius-lg h-100">
+              <div class="">
                 <img src="/soft-ui-dashboard-main/assets/img/shapes/waves-white.svg" class="position-absolute h-100 w-50 top-0 d-lg-block d-none" alt="waves">
                 <div class="position-relative d-flex align-items-center justify-content-center h-100">
-                  <img class="w-100 position-relative z-index-2 pt-4" src="/soft-ui-dashboard-main/assets/img/illustrations/rocket-white.png" alt="rocket">
+                  <img class="w-100 position-relative z-index-2 pt-4" src="https://suniindonesia.com/wp-content/uploads/2022/07/LOGO-2-2@2x.png" alt="rocket">
                 </div>
               </div>
             </div>
@@ -77,17 +71,14 @@
         </div>
       </div>
     </div>
-    <div class="col-lg-5">
+    <div class="col-lg-4">
       <div class="card h-100 p-3">
-        <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('/soft-ui-dashboard-main/assets/img/ivancik.jpg');">
-          <span class="mask bg-gradient-dark"></span>
+        <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100"> 
+          {{-- style="background-image: url('/soft-ui-dashboard-main/assets/img/ivancik.jpg');" --}}
+        <span class="mask bg-gradient-dark"></span>
           <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
-            <h5 class="text-white font-weight-bolder mb-4 pt-2">Work with the rockets</h5>
-            <p class="text-white">Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
-            <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="">
-              Read More
-              <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
-            </a>
+            <h5 class="text-white font-weight-bolder mb-4 pt-2 fs-1">{{ $hijriDateday }} {{ $hijriDatemonth }}</h5>
+            <p class="text-white">{{ $hijriDateyear }} Hijriyah</p>
           </div>
         </div>
       </div>
