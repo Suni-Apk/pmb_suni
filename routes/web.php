c702aa4411222d4590e185db7544e056db903b03
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-10-26 11:08:51 +0700

    update yuum

--- routes/web.php
+++ routes/web.php
@@ -116,7 +116,3 @@ Route::prefix('template')->group(function () {
         return view('layouts.template.wizard');
     })->name('wizard');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
