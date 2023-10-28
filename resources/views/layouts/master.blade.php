b68a003e4ae86c3fa185e70c7e863268bca6e559
haidaryuum <120004849+yuumuu@users.noreply.github.com>
2023-10-28 13:39:35 +0700

    bismillah

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     @stack('styles')
 </head>
 
-<body class="g-sidenav-show bg-gray-100 g-sidenav-hidden">
+<body class="g-sidenav-show bg-gray-100 g-sidenav-hidden overflow-x-hidden">
     @include('layouts.aside')
 
     <main class="main-content position-relative min-height-screen h-auto border-radius-lg">
