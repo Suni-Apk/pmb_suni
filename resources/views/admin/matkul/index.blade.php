46e3398b5282bba2241af524ee2f88c8839a5162
Alegrich <140672362+Alegrich@users.noreply.github.com>
2023-10-30 09:40:33 +0700

    benerin tipis tipis

--- resources/views/admin/matkul/index.blade.php
+++ resources/views/admin/matkul/index.blade.php
@@ -80,7 +80,7 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm bg-gradient-danger font-weight-bold text-xs mx-2 show_confirm mt-3">
-                                                <i class="fa fa-trash fs-6"></i>
+                                                <i class="fa fa-trash fs-6 show_confirm"></i>
                                             </button>
                                         </form>
                                     </td>
@@ -106,4 +106,52 @@
             fixedHeight: true,
         });
     </script>
+    <script src="sweetalert2.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script>
+        @if (Session::has('success'))
+            toastr.success("{{ Session::get('success') }}")
+        @endif
+
+        @if (Session::has('delete'))
+            toastr.success("{{ Session::get('success') }}")
+        @endif
+
+        @if (Session::has('pesan'))
+            toastr.error('{{ Session::get('pesan') }}')
+        @endif
+    </script>
+    <script type="text/javascript">
+        $('.show_confirm').click(function(event) {
+            var form = $(this).closest("form");
+            event.preventDefault();
+            Swal.fire({
+                title: 'Yakin?',
+                text: "Kamu Akan Menghapus Mata Kuliah!!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Iya,  Hapus!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                    Swal.fire(
+                        'Terhapus!',
+                        'Kamu telah menghapus Mata Kuliah!!.',
+                        'success'
+                    )
+                }
+            });
+        });
+    </script>
 @endpush
+
